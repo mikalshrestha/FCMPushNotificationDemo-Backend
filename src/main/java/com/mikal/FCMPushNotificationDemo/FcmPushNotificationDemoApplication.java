e2595d707cef5c754cec48d4e0a5a38fd806c929
Mikal Shrestha <mikalshrestha@Mikals-MacBook-Pro.local>
2021-07-26 09:15:26 +0545

    initial commit

---
+++ src/main/java/com/mikal/FCMPushNotificationDemo/FcmPushNotificationDemoApplication.java
@@ -0,0 +1,13 @@
+package com.mikal.FCMPushNotificationDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FcmPushNotificationDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FcmPushNotificationDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mikal/FCMPushNotificationDemo/FcmPushNotificationDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mikal.FCMPushNotificationDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FcmPushNotificationDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
