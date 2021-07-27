11f56455b506fc06c4bb3bf9eebf8689ca5b8a9e
Mikal Shrestha <mikalshrestha@Mikals-MacBook-Pro.local>
2021-07-27 08:55:21 +0545

    minor cross origin issue fixes

--- src/main/java/com/mikal/FCMPushNotificationDemo/controller/FCMPushNotificationController.java
+++ src/main/java/com/mikal/FCMPushNotificationDemo/controller/FCMPushNotificationController.java
@@ -4,12 +4,14 @@ import com.mikal.FCMPushNotificationDemo.model.PushNotificationRequest;
 import com.mikal.FCMPushNotificationDemo.model.PushNotificationResponse;
 import com.mikal.FCMPushNotificationDemo.service.NotificationService;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
+@CrossOrigin
 public class FCMPushNotificationController {
 
     private NotificationService notificationService;
@@ -18,10 +20,9 @@ public class FCMPushNotificationController {
         this.notificationService = notificationService;
     }
 
-    @PostMapping("/notification/token")
+    @PostMapping("/send/notification")
     public ResponseEntity sendTokenNotification(@RequestBody PushNotificationRequest request) {
         notificationService.sendPushNotificationToToken(request);
-        System.out.println("princr");
         return new ResponseEntity<>(new PushNotificationResponse(HttpStatus.OK.value(), "Notification has been sent."), HttpStatus.OK);
     }
 }
